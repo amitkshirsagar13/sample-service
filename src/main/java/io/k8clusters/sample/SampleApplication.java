1b302030c5fe984003572a70eb5e8f665fa384b4
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2021-08-22 13:32:27 +0530

    added test

--- src/main/java/io/k8clusters/sample/SampleApplication.java
+++ src/main/java/io/k8clusters/sample/SampleApplication.java
@@ -14,6 +14,5 @@ public class SampleApplication {
     @RequestMapping("/")
     public String index() {
         return "Hello, Unfeatured Sample Spring Boot!!!";
-        x
     }
 }
--- src/test/java/io/k8clusters/sample/SampleApplicationTests.java
+++ src/test/java/io/k8clusters/sample/SampleApplicationTests.java
@@ -1,20 +1,44 @@
 package io.k8clusters.sample;
 
+import io.k8clusters.sample.models.Message;
+import io.k8clusters.sample.services.GreetingService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
 @Slf4j
-class SampleApplicationTests {
+public class SampleApplicationTests {
+
+    private GreetingService greetingService;
+
+    @BeforeTestClass
+    public static void staticInit(){
+        log.info("Initialized SampleApplicationTests");
+    }
+
+    @BeforeEach
+    public void init() throws Exception{
+        log.info("Initialize Mocks");
+        greetingService = mock(GreetingService.class);
+        when(greetingService.greetSomeone(anyString(), anyString())).thenCallRealMethod();
+    }
 
     @Test
     void contextLoads() {
-        log.info("----------------");
-        log.info("Run some test!!!");
-        log.info("----------------");
-        log.info("Ran some test!!!");
-        log.info("----------------");
-    }
+        String name = "Groot";
+        String greeting = "I am groot";
 
+        Message message = greetingService.greetSomeone(name, greeting);
+        assertNotNull(message);
+        log.info("Message: {}", message.greetings());
+        assertEquals("I am groot, Groot!!!", message.greetings());
+    }
 }
