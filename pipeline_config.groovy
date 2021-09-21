653919fddf1582ab1794e91656432098de32bb58
Amit Kshirsagar <amitkshirsagar13@users.noreply.github.com>
2021-09-21 21:46:06 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,3 +1,15 @@
 libraries {
+  ansible{
+    nodeLabel = "linux"
+  }
   maven 
+}
+
+application_environments {
+  dev {
+    ip_addresses = ["192.168.1.111", "192.168.1.115"]
+  }
+  test {
+    ip_addresses = ["10.0.2.111", "10.0.2.115"]
+  }
 }
